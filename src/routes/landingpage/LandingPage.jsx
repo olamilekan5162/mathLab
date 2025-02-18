9fb07d7d623735a3e727ddb4b1bc57086c63051c
olamilekan5162 <opeyemiolalekan481@gmail.com>
2025-02-18 19:31:07 +0100

    Work on the Landing page

--- src/routes/landingpage/LandingPage.jsx
+++ src/routes/landingpage/LandingPage.jsx
@@ -1,15 +1,41 @@
 import React, {useState} from "react"
+import './LandingPage.css'
 import {Link} from "react-router-dom"
+import Mathnw from '../../assets/Mathnw.png'
+import Thomas from '../../assets/jeswin-thomas.jpg'
 
 const LandingPage = () => {
     return(
-        <div>
-          <Link to="/login">Login here</Link>
-          <Link to="/register">Register here</Link>
-          <Link to="/dashboard">The dashboard</Link>
-            <p>Body</p>
-        </div>
+        <>
+          <section className="navbar">
+              <div className="logo">
+                   <img src={Mathnw} alt="logo" />
+              </div>
+      
+              <div className="navbtn">
+                  <Link to="/login">Log In</Link>
+                  <Link to="/register">Sign Up</Link>
+              </div>
+          </section>
+      
+          <section className="main">
+              <div className="main-body">
+                  <div>
+                      <h1>Master Maths Like a Pro</h1>
+                      <p>Challenge yourself and level up your math skills! Sign up for FREE and get AI-generated questions that adapt to your progress in real time!</p>
+                  </div>
+      
+                  <div>
+                      <Link className="btn" to="/register">Sign Up</Link>
+                  </div>
+              </div>
+      
+              <div className="main-img">
+                  <img src={Thomas} alt="math_image" className="img2" />
+              </div>
+          </section>
+        </>
         )
 }
 
-export default LandingPage
+export default LandingPage;
